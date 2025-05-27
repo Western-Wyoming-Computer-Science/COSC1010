c1bb1494445b5c5a3977b59a7649755e9b4773a4
Tammy Robertson Callahan <80910439+DrTammyRobertson@users.noreply.github.com>
2025-05-27 15:54:53 -0700

    Add files via upload

---
+++ Sales-Prediction/main.py
@@ -0,0 +1,14 @@
+#
+# Name
+# Date
+# Sales Prediction Programming Project
+# COSC 2409 DNT
+#
+
+# Variables to hold the sales total and the profit
+
+# Get the amount of projected sales.
+
+# Calculate the projected profit.
+
+# Print the projected profit.
---
+++ Your-First-Python-Program/main.py
@@ -0,0 +1,8 @@
+#
+# Name
+# Date
+# Your First Python Program
+# COSC 1010 
+#
+
+# This is my first Python program.
